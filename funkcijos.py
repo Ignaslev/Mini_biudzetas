77f4e9de8723d4057b09d6a66824d3666fab03f3
ignaslev <levinskas.ignas@gmail.com>
2024-11-29 10:34:31 +0200

    > Sukurtas main failas su while loop > Sukurtas funkciju file su tusciom reikiamom funkcijom > Funkcijos prijungtos prie main

---
+++ funkcijos.py
@@ -0,0 +1,17 @@
+import datetime
+import pickle
+
+def ivesti_pajams():
+    pass
+
+def ivesti_islaidas():
+    pass
+
+def ziureti_pajamas():
+    pass
+
+def ziureti_islaidas():
+    pass
+
+def statistika():
+    pass
---
+++ main.py
@@ -0,0 +1,36 @@
+import funkcijos
+
+def main():
+    print('Sveiki atvyke!')
+    while True:
+        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
+        print('| 1. Ivesti pajamas             |\n'
+              '| 2. Ivesti islaidas            |\n'
+              '| 3. Ziureti pajamas            |\n'
+              '| 4. Ziureti islaidas           |\n'
+              '| 5. Statistika                 |\n'
+              '| q - iseiti                    |')
+        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
+        pasirinkimas = input('Pasirinkimas: ')
+
+        if pasirinkimas == '1':  # ivesti pajamas
+            pass
+
+        if pasirinkimas == '2':   # ivesti islaidas
+            pass
+
+        if pasirinkimas == '3':   # ziureti pajamas
+            pass
+
+        if pasirinkimas == '4':   # ziureti islaidas
+            pass
+
+        if pasirinkimas == '5':   # statistika
+            pass
+
+        if pasirinkimas == 'q':   # iseiti
+            break
+
+
+if __name__ == '__main__':
+    main()
